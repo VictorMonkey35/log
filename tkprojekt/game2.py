e29017b6eb2c27e21ecc546119a756e57502cf86
VictorMonkey35 <victor.forseback@skola.taby.se>
2021-12-16 09:38:47 +0100

    21-12-16

--- tkprojekt/game2.py
+++ tkprojekt/game2.py
@@ -1,21 +1,74 @@
 import tkinter as tk
+import time as t
 
 arena = tk.Tk()
 arena.geometry('900x540')
 
 rockimg = tk.PhotoImage(file = 'tkprojekt/bilder/rock.png')
+rockimg = rockimg.subsample(7,7)
 paperimg = tk.PhotoImage(file = 'tkprojekt/bilder/paper.png')
+paperimg = paperimg.subsample(11,11)
 scissorsimg = tk.PhotoImage(file = 'tkprojekt/bilder/scissors.png')
+scissorsimg = scissorsimg.subsample(10,10)
 tableimg = tk.PhotoImage(file = 'tkprojekt/bilder/table.png')
 jonasimg = tk.PhotoImage(file = 'tkprojekt/bilder/Jonas.png')
 
-canvas = tk.Canvas(arena, width = 900, height = 540, bg = 'grey')
+rockY = 450
+paperY = 450
+scissorsY = 450
+
+rvar = 0
+pvar = 0
+svar = 0
+
+canvas = tk.Canvas(arena, width = 900, height = 540, bg = 'grey', bd = 0)
 canvas.pack()
 
-canvas.create_image(450, 240, anchor = tk.CENTER, image = jonasimg)
-canvas.create_image(450, 600, anchor = tk.CENTER, image = tableimg)
-canvas.create_image(450, 400, anchor = tk.CENTER, image = rockimg)
-canvas.create_image(450, 400, anchor = tk.CENTER, image = paperimg)
-canvas.create_image(450, 400, anchor = tk.CENTER, image = scissorsimg)
+def rockf():
+    global rvar, pvar, svar
+    rvar = 1
+    pvar = 0
+    svar = 0
+
+def paperf():
+    global rvar, pvar, svar
+    rvar = 0
+    pvar = 1
+    svar = 0
+
+def scissorsf():
+    global rvar, pvar, svar
+    rvar = 0
+    pvar = 0
+    svar = 1
+
+def draw():
+    global rockY, paperY, scissorsY
+    canvas.create_image(450, 240, anchor = tk.CENTER, image = jonasimg)
+    canvas.create_image(450, 600, anchor = tk.CENTER, image = tableimg)
+    canvas.create_image(200, rockY, anchor = tk.CENTER, image = rockimg)
+    canvas.create_image(450, paperY, anchor = tk.CENTER, image = paperimg)
+    canvas.create_image(700, scissorsY, anchor = tk.CENTER, image = scissorsimg)
+    rockY = 450 - 8 * rvar
+    paperY = 450 - 8 * pvar
+    scissorsY = 450 - 8 * svar
+
+
+def gameloop():
+    canvas.delete("all")
+    draw()
+    canvas.update()
+    arena.after(17, func = gameloop)
+
+rockb = tk.Button(arena, text = 'play rock', bd = 1, command = rockf)
+rockb.place(anchor = tk.CENTER, x = 200, y = 517)
+
+paperb = tk.Button(arena, text = 'play paper', bd = 1, command = paperf)
+paperb.place(anchor = tk.CENTER, x = 450, y = 517)
+
+scissorsb = tk.Button(arena, text = 'play scissors', bd = 1, command = scissorsf)
+scissorsb.place(anchor = tk.CENTER, x = 700, y = 517)
+
+gameloop()
 
 arena.mainloop()
--- tkprojekt/test.py
+++ tkprojekt/test.py
@@ -1,29 +1,43 @@
-# importing only those functions which
-# are needed
-from tkinter import Tk, mainloop, TOP
-from tkinter.ttk import Button
- 
-# time function used to calculate time
-from time import time
- 
-# creating tkinter window
-root = Tk()
- 
-button = Button(root, text = 'Geeks')
-button.pack(side = TOP, pady = 5)
- 
-print('Running...')
-# Calculating starting time
-start = time()
- 
-# in after method 5000 milliseconds
-# is passed i.e after 5 seconds
-# main window i.e root window will
-# get destroyed
-root.after(5000, root.destroy)
- 
-mainloop()
- 
-# calculating end time
-end = time()
-print('Destroyed after % d seconds' % (end-start))
+import tkinter as tk
+import time
+import random as rand
+x = tk.Tk()
+c = tk.Canvas(x, bg="white", height=720, width=1280)
+c.pack()
+
+arrowImg = tk.PhotoImage(file='./projekt/bowsimulator/arrow1.png')
+board = tk.PhotoImage(file='./projekt/bowsimulator/board.png')
+
+bowAnimation = [
+    tk.PhotoImage(file='./projekt/bowsimulator/bow1.png'), 
+    tk.PhotoImage(file='./projekt/bowsimulator/bow2.png'), 
+    tk.PhotoImage(file='./projekt/bowsimulator/bow3.png'),
+    tk.PhotoImage(file='./projekt/bowsimulator/bow4.png')]
+
+arrowX = 100
+arrowY = 500
+arrowYVel = 5
+run = True
+
+
+def gameloop():
+    global arrowX
+    global arrowY
+    global arrowYVel
+    global run
+    c.create_image(100, 500, image=bowAnimation[rand.randint(0,3)])
+    c.create_image(arrowX, arrowY, image=arrowImg)
+
+    arrowX = arrowX + 10
+    arrowYVel = arrowYVel - 0.1
+    arrowY = arrowY + -arrowYVel
+    if arrowX >= 1200:
+        run = False
+    if run:
+        c.update()
+        time.sleep(1/60)
+        c.delete("all")
+        gameloop()
+
+gameloop()
+x.mainloop()
