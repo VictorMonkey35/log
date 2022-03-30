86b82f9191ce4463b048216aa6d467c8f45a6b12
VictorMonkey35 <89571590+VictorMonkey35@users.noreply.github.com>
2022-03-30 22:19:01 +0200

    Add files via upload

---
+++ slutarbete/client.py
@@ -0,0 +1,151 @@
+#Adala Airlines
+
+#registrera/logga in
+#logga in som admin för att lägga till flights
+
+#städer som klasser
+#stockholm
+#madrid
+#paris
+#new york
+#rio de janeiro
+#tokyo
+#dubai
+#kairo
+
+#flygresor som klasser, olika säten som sub klasser
+
+#passagerare som klasser, med namn och ålder
+
+#economy class, adala plus, bussines class
+
+import tkinter as tk
+import time as t
+import _thread as th
+import socket as so
+
+tickval = 0
+menu = True
+
+c = tk.Tk()
+
+adalaLogo1 = tk.PhotoImage(file = 'slutarbete/bilder/adalaLogo1.png')
+madrid = tk.PhotoImage(file = 'slutarbete/bilder/madrid.png')
+newyork = tk.PhotoImage(file = 'slutarbete/bilder/newyork.png')
+rio = tk.PhotoImage(file = 'slutarbete/bilder/rio.png')
+whitebox = tk.PhotoImage(file = 'slutarbete/bilder/whitebox.png')
+
+rio = rio.zoom(1,7)
+rio = rio.subsample(1,6)
+whitebox = whitebox.subsample(6,5)
+
+def register():
+    usernametitle.place_forget()
+    username.place_forget()
+    passwordtitle.place_forget()
+    password.place_forget()
+    loginb.place_forget()
+    registertxt.place_forget()
+    registerb.place_forget()
+
+    usernametitle.place(anchor = tk.CENTER , x = 415 , y = 215)
+    username.place(anchor = tk.CENTER , x = 480 , y = 235)
+    fnametitle.place(anchor = tk.CENTER , x = 415 , y = 255)
+    fname.place(anchor = tk.CENTER , x = 480 , y = 275)
+    lnametitle.place(anchor = tk.CENTER , x = 415 , y = 295)
+    lname.place(anchor = tk.CENTER , x = 480 , y = 315)
+    passwordtitle.place(anchor = tk.CENTER , x = 415 , y = 335)
+    password.place(anchor = tk.CENTER , x = 480 , y = 355)
+    cAccountb.place(anchor = tk.CENTER , x = 480 , y = 395)
+    backb.place(anchor = tk.CENTER , x = 330 , y = 85)
+
+def login():
+    global menu
+    usernametitle.place_forget()
+    username.place_forget()
+    passwordtitle.place_forget()
+    password.place_forget()
+    loginb.place_forget()
+    registertxt.place_forget()
+    registerb.place_forget()
+    menu = False
+
+def loginscreen():
+    usernametitle.place_forget()
+    username.place_forget()
+    fnametitle.place_forget()
+    fname.place_forget()
+    lnametitle.place_forget()
+    lname.place_forget()
+    passwordtitle.place_forget()
+    password.place_forget()
+    cAccountb.place_forget()
+    backb.place_forget()
+
+    usernametitle.place(anchor = tk.CENTER , x = 415 , y = 215)
+    username.place(anchor = tk.CENTER , x = 480 , y = 235)
+    passwordtitle.place(anchor = tk.CENTER , x = 415 , y = 255)
+    password.place(anchor = tk.CENTER , x = 480 , y = 275)
+    loginb.place(anchor = tk.CENTER , x = 480 , y = 315)
+    registertxt.place(anchor = tk.CENTER , x = 455 , y = 440)
+    registerb.place(anchor = tk.CENTER , x = 544 , y = 441)
+
+
+canvas = tk.Canvas(c , bg = 'white' , width = 960 ,  height = 600 , bd = 0)
+canvas.pack()
+
+usernametitle = tk.Label(c , text = 'Username' , bd = 0 , bg = 'white')
+username = tk.Entry(c , width = 30 , bd = 0 , bg = 'lightgrey')
+
+passwordtitle = tk.Label(c , text = 'Password' , bg = 'white' , bd = 0)
+password = tk.Entry(c , width = 30 , bd = 0 , bg = 'lightgrey')
+
+loginb = tk.Button(c , text = 'Log in' , bd = 0 , command = login)
+cAccountb = tk.Button(c , text = 'Create account' , bd = 0 , command = loginscreen)
+backb = tk.Button(c , text = '<-' , bd = 0 , command = loginscreen , bg = 'white')
+
+registertxt = tk.Label(c , text = 'Dont have an account?' , bd = 0 , bg = 'white')
+registerb = tk.Button(c , text = 'Register!' , bd = 0 , bg = 'white' , command = register , fg = 'blue')
+
+fnametitle = tk.Label(c , text = 'First name' , bd = 0 , bg = 'white')
+fname = tk.Entry(c , width = 30 , bd = 0 , bg = 'lightgrey')
+
+lnametitle = tk.Label(c , text = 'Last name' , bd = 0 , bg = 'white')
+lname = tk.Entry(c , width = 30 , bd = 0 , bg = 'lightgrey')
+
+usernametitle.place(anchor = tk.CENTER , x = 415 , y = 215)
+username.place(anchor = tk.CENTER , x = 480 , y = 235)
+passwordtitle.place(anchor = tk.CENTER , x = 415 , y = 255)
+password.place(anchor = tk.CENTER , x = 480 , y = 275)
+loginb.place(anchor = tk.CENTER , x = 480 , y = 315)
+registertxt.place(anchor = tk.CENTER , x = 455 , y = 440)
+registerb.place(anchor = tk.CENTER , x = 544 , y = 441)
+
+def tick():
+    global tickval
+    if tickval == 2880:
+        tickval = 0
+    else:
+        tickval += 1
+
+def draw():
+    if menu == True:
+        canvas.create_image(480 - tickval , 310 , anchor = tk.CENTER , image = madrid)
+        canvas.create_image(1440 - tickval , 310 , anchor = tk.CENTER , image = newyork)
+        canvas.create_image(2400 - tickval , 310 , anchor = tk.CENTER , image = rio)
+        canvas.create_image(3360 - tickval , 310 , anchor = tk.CENTER , image = madrid)
+
+        canvas.create_image(480 , 270 , anchor = tk.CENTER , image = whitebox)
+        canvas.create_image(480 , 155 , anchor = tk.CENTER , image = adalaLogo1)
+
+def loop():
+    canvas.delete('all')
+    draw()
+    if menu == True:
+        tick()
+    canvas.update()
+    c.after(17 , func = loop)
+
+loop()
+
+c.mainloop()
