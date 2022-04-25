928c1394ed242bf13dd26b1089192a739ddf7adf
VictorMonkey35 <89571590+VictorMonkey35@users.noreply.github.com>
2022-04-25 15:20:32 +0200

    Add files via upload
    22-04-25
    fungerande datum selection samt location selection för bokningen.

--- slutarbete/client.py
+++ slutarbete/client.py
@@ -105,18 +105,21 @@ def dateup():
     global date
     if date == 12:
         date = 1
+        dtext.config(text = months[date-1])
     
     else:
         date += 1
-
+        dtext.config(text = months[date-1])
 
 def datedown():
     global date
     if date == 1:
         date = 12
+        dtext.config(text = months[date-1])
     
     else:
-        date -= 1  
+        date -= 1
+        dtext.config(text = months[date-1])
     
 
 
@@ -142,19 +145,61 @@ fname = tk.Entry(c , width = 30 , bd = 0 , bg = 'lightgrey')
 lnametitle = tk.Label(c , text = 'Last name' , bd = 0 , bg = 'white')
 lname = tk.Entry(c , width = 30 , bd = 0 , bg = 'lightgrey')
 
+
+#deklarerar elementen för "top info rutan"
 mFrame = tk.Frame(c , bg = 'orange' , bd = 0 , height = 60 , width = 964)
 
-lFrame = tk.Frame(c , bg = 'green' , bd = 0 , height = 540 , width = 210)
 
+#deklarerar elementen för "location rutan"
+LocationVarF1 = tk.IntVar()
+LocationVarF2 = tk.IntVar()
+LocationVarF3 = tk.IntVar()
+LocationVarF4 = tk.IntVar()
+LocationVarF5 = tk.IntVar()
+LocationVarF6 = tk.IntVar()
+
+LocationVarT1 = tk.IntVar()
+LocationVarT2 = tk.IntVar()
+LocationVarT3 = tk.IntVar()
+LocationVarT4 = tk.IntVar()
+LocationVarT5 = tk.IntVar()
+LocationVarT6 = tk.IntVar()
+
+lFrame = tk.Frame(c , bg = 'green' , bd = 0 , height = 540 , width = 210)
+toText = tk.Label(lFrame , bg = 'white' , bd = 0 , text = 'From')
+fromText = tk.Label(lFrame , bg = 'white' , bd = 0 , text = 'From')
+
+flb1 = tk.Checkbutton(lFrame, text = "Stockholm", variable = LocationVarF1, onvalue = 1, offvalue = 0)
+flb2 = tk.Checkbutton(lFrame, text = "Madrid", variable = LocationVarF2, onvalue = 1, offvalue = 0)
+flb3 = tk.Checkbutton(lFrame, text = "Paris", variable = LocationVarF3, onvalue = 1, offvalue = 0)
+flb4 = tk.Checkbutton(lFrame, text = "Dubai", variable = LocationVarF4, onvalue = 1, offvalue = 0)
+flb5 = tk.Checkbutton(lFrame, text = "New York", variable = LocationVarF5, onvalue = 1, offvalue = 0)
+flb6 = tk.Checkbutton(lFrame, text = "Tokyo", variable = LocationVarF6, onvalue = 1, offvalue = 0)
+
+tlb1 = tk.Checkbutton(lFrame, variable = LocationVarT1, onvalue = 1, offvalue = 0)
+tlb2 = tk.Checkbutton(lFrame, variable = LocationVarT2, onvalue = 1, offvalue = 0)
+tlb3 = tk.Checkbutton(lFrame, variable = LocationVarT3, onvalue = 1, offvalue = 0)
+tlb4 = tk.Checkbutton(lFrame, variable = LocationVarT4, onvalue = 1, offvalue = 0)
+tlb5 = tk.Checkbutton(lFrame, variable = LocationVarT5, onvalue = 1, offvalue = 0)
+tlb6 = tk.Checkbutton(lFrame, variable = LocationVarT6, onvalue = 1, offvalue = 0)
+
+
+#deklarerar elementen för "biljettklass rutan"
 cFrame = tk.Frame(c , bg = 'blue' , bd = 0 , height = 80 , width = 460)
 
+
+#deklarerar elementen för "datum rutan"
 dFrame = tk.Frame(c , bg = 'red' , bd = 0 , height = 540 , width = 300)
 bdbutton = tk.Button(dFrame , text = '<-' , command = datedown , bd = 0 , bg = 'white')
 fdbutton = tk.Button(dFrame , text = '->' , command = dateup , bd = 0 , bg = 'white')
 dtext = tk.Label(dFrame , text = months[date-1])
 
+
+#deklarerar elementen för "sökresultat rutan"
 fFrame = tk.Frame(c , bg = 'pink' , bd = 0 , height = 460 , width = 460)
 
+
+#placerar ut alementen för loggin sidan
 usernametitle.place(anchor = tk.CENTER , x = 415 , y = 215)
 username.place(anchor = tk.CENTER , x = 480 , y = 235)
 passwordtitle.place(anchor = tk.CENTER , x = 415 , y = 255)
@@ -163,6 +208,8 @@ loginb.place(anchor = tk.CENTER , x = 480 , y = 315)
 registertxt.place(anchor = tk.CENTER , x = 455 , y = 440)
 registerb.place(anchor = tk.CENTER , x = 544 , y = 441)
 
+
+#gör så att variabeln tickvar ökar med 1 i värde, variablen behandlar positionen för bakgrundsbilderna.
 def tick():
     global tickval
     if tickval == 2880:
@@ -170,6 +217,8 @@ def tick():
     else:
         tickval += 1
 
+
+#innehåller allt som ska ske i loopen, alltså ritas var 17:e sekund.
 def draw():
     if menu == True:
         canvas.create_image(480 - tickval , 310 , anchor = tk.CENTER , image = madrid)
@@ -182,7 +231,9 @@ def draw():
     
     if booking == True:
         pass
+ 
 
+ #ser till att programmet uppdateras var 17:e millisekund.
 def loop():
     canvas.delete('all')
     draw()
@@ -191,10 +242,26 @@ def loop():
     canvas.update()
     c.after(17 , func = loop)
 
+
+#bygger upp hemsidan.
 def start():
     mFrame.place(anchor = tk.NW , x = 0 , y = 0)
 
     lFrame.place(anchor = tk.NW , x = 0 , y = 60)
+    toText.place(anchor = tk.CENTER , x = 20 , y = 20)
+    fromText.place(anchor = tk.CENTER , x = 60 , y = 20)
+    flb1.place(anchor = tk.NW , x = 80 , y = 40)
+    flb2.place(anchor = tk.NW , x = 80 , y = 60)
+    flb3.place(anchor = tk.NW , x = 80 , y = 80)
+    flb4.place(anchor = tk.NW , x = 80 , y = 100)
+    flb5.place(anchor = tk.NW , x = 80 , y = 120)
+    flb6.place(anchor = tk.NW , x = 80 , y = 140)
+    tlb1.place(anchor = tk.NW , x = 160 , y = 40)
+    tlb2.place(anchor = tk.NW , x = 160 , y = 60)
+    tlb3.place(anchor = tk.NW , x = 160 , y = 80)
+    tlb4.place(anchor = tk.NW , x = 160 , y = 100)
+    tlb5.place(anchor = tk.NW , x = 160 , y = 120)
+    tlb6.place(anchor = tk.NW , x = 160 , y = 140)
 
     cFrame.place(anchor = tk.NW , x = 210 , y = 60)
 
@@ -205,6 +272,10 @@ def start():
 
     fFrame.place(anchor = tk.NW , x = 210 , y = 140)
 
+
+#startar loopen
 loop()
 
+
+#markerar slutet för tk-fönstret
 c.mainloop()
